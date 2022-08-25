3e8748728c84a44cc842ba406dcbb25412b1a886
IlyaVostrukhin <i.s.vostrukhin@yandex.ru>
2022-08-25 16:26:02 +0700

    Initial commit

---
+++ src/main/java/com/projects/backend/rutube2/Rutube2BackendApplication.java
@@ -0,0 +1,13 @@
+package com.projects.backend.rutube2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rutube2BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Rutube2BackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projects/backend/rutube2/Rutube2BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.backend.rutube2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Rutube2BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
